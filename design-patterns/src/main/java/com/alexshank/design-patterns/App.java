4d6a26d325012610d01a7e3020f7f2a5f973e4f2
Alex Shank <ashank31@gmail.com>
2022-01-04 19:14:55 -0600

    Created initial maven project.

---
+++ design-patterns/src/main/java/com/alexshank/design-patterns/App.java
@@ -0,0 +1,13 @@
+package com.alexshank.design-patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
